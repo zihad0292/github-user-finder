8b73e73908d325b8f1b44c9416f747167a48d1da
zihad0292 <zihad0292@gmail.com>
2020-09-30 15:33:43 +0600

    Fixes api call ensuring the correct username

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -4,18 +4,18 @@ import PropTypes from "prop-types";
 
 const UserItem = ({ user: { avatar_url, login } }) => {
   return (
-    <div className='card text-center'>
+    <div className="card text-center">
       <img
         src={avatar_url}
-        alt=''
-        className='round-img'
+        alt=""
+        className="round-img"
         style={{ width: "60px" }}
       />
       <h3>{login}</h3>
       <div>
         <Link
           to={`/users/${login}`}
-          className='btn btn-dark btn-small my-1'
+          className="btn btn-dark btn-small my-1"
           style={{ marginRight: "0" }}
         >
           More
@@ -26,7 +26,7 @@ const UserItem = ({ user: { avatar_url, login } }) => {
 };
 
 UserItem.propTypes = {
-  user: PropTypes.object.isRequired
+  user: PropTypes.object.isRequired,
 };
 
 export default UserItem;
