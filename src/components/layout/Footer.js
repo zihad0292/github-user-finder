b1593f770249fff81008febcd413ed68ed2d36f6
zihad0292 <zihad0292@gmail.com>
2020-10-03 15:36:25 +0600

    Changes app title and footer credit link

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,10 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='bg-light all-center py-1' style={{ marginTop: "15px" }}>
+    <footer className="bg-light all-center py-1" style={{ marginTop: "15px" }}>
       <p>
         Created by{" "}
-        <a href='https://bd.linkedin.com/in/zihad-ul-islam'>Zihad Ul Islam</a>
+        <a
+          href="https://zihadsweb.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Zihad Ul Islam
+        </a>
       </p>
     </footer>
   );
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,26 +4,26 @@ import PropTypes from "prop-types";
 
 const Navbar = ({ title, icon }) => {
   return (
-    <nav className='navbar bg-primary'>
+    <nav className="navbar bg-primary">
       <h1>
         <i className={icon} /> {title}
       </h1>
       <ul>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
       </ul>
     </nav>
   );
 };
 
 Navbar.defaultProps = {
-  title: "Github User Finder",
-  icon: "fab fa-github"
+  title: "Github User Explorer",
+  icon: "fab fa-github",
 };
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired
+  icon: PropTypes.string.isRequired,
 };
 
 export default Navbar;
